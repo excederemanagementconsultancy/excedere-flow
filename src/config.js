93c238d6c6ccd5232a0e1d8ec1a6393f6ee54733
excederemanagementconsultancy <jonpollard24271@gmail.com>
2026-09-19 21:14:27 -0400

    Connect Flow account service

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // Public browser configuration only. Never put a service-role key or password here.
-export const accountConfig = { url: '', publishableKey: '' };
+export const accountConfig = { url: 'https://ydgbfupdyibayndvqamq.supabase.co', publishableKey: 'sb_publishable__-trTGzCx4SsNb6Knilcag__3oiv71f' };
